d762306382d9a16dcf48a2909a611ec8d93b6921
felixH3r <58719843+felixH3r@users.noreply.github.com>
2021-05-11 15:12:29 +0200

    Routing

--- waterMe/src/app/app.module.ts
+++ waterMe/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponent } from './home/home.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
